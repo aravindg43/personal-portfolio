cd1eb1d21fa83ae0af155ee5d8fcb549729846b9
aravind_infopine <aravindgcept@gmail.com>
2025-06-13 17:18:19 +0530

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <Image
+          className="mx-2 w-24 md:w-40 h-auto"
+          src="/assets/MyLogo.png"
+          alt="logo"
+          width={120}
+          height={40}
+        />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/srijalprasad/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="w-6 h-6 md:w-8 md:h-8" />
+        </a>
+        <a href="https://github.com/Srijal-prasad" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="w-6 h-6 md:w-8 md:h-8" />
+        </a>
+        {/* <a href="https://x.com/PrasadSrijal" target="_blank" rel="noopener noreferrer">
+          <FaTwitter className="w-6 h-6 md:w-8 md:h-8" />
+        </a>
+        <a href="https://www.instagram.com/srijal_prasad/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="w-6 h-6 md:w-8 md:h-8" />
+        </a> */}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,68 @@
+export const HERO_CONTENT = `I am a passionate FrontEnd developer with a talent for crafting responsive and maintainable web applications.With 3+ years of hands-on experience in modern JavaScript frameworks like React, Next, Redux, and a strong background in integrating RESTful APIs, I aim to leverage my skills to create innovative and user-centric solutions that enhance productivity and user engagement.`;
+export const ABOUT_TEXT = `Hi, I'm Aravind G, a Software Developer based in Mysore. I enjoy building web applications that are responsive, easy to use, and built to scale. I mainly work with React.js, Next.js, and Tailwind CSS, and I focus on creating clean, user-friendly interfaces that help improve how people interact with software.
+I'm currently working at CEPT, Department of Posts (DoP), where I'm part of the team behind the India Post Delivery and Postman Management System. This project is now used in over 150,000 post offices across India. I’ve worked on several key modules like Shift Management, Beat Mapping, and eMO Handling, and I’ve helped improve the app’s performance using CI/CD pipelines with Jenkins and GitLab.
+Before this, I worked on a Distribution Management System (DMS) for the healthcare industry. It was built to help hospitals and distributors manage surgery implant inventory in real-time, reducing manual work and improving coordination.
+I graduated with a degree in Computer Science from East Point College of Engineering and Technology, and over the years, I’ve gained experience working in Agile teams, collaborating on feature development, and writing maintainable, reusable code.
+When I’m not coding, I enjoy learning about new tech, trying out tools, and improving my skills one project at a time`;
+
+export const EXPERIENCES = [
+  {
+    year: "Sept 2024 - Present",
+    role: "Senior Software Engineer",
+    company: "Visionet Systems",
+    description: `Architected and developed dynamic, server-rendered components with Next.js, enhancing application scalability and improving load times by 40%. Designed and implemented reusable UI components using React.js and Tailwind CSS, streamlining development workflows and boosting maintainability. Optimized complex state management with Redux Toolkit, ensuring seamless synchronization across features. Diagnosed and resolved critical performance bottlenecks by leveraging systematic debugging and performance profiling, achieving a 30% improvement in overall application stability.`,
+    technologies: ["Javascript", "Next.js", "Redux", "Airtable", "Storybook"],
+  },
+  {
+    year: "May 2021 - Oct 2023",
+    role: "Application Development Analyst",
+    company: "Accenture Solutions Pvt. Ltd.",
+    description: `Architected and integrated a suite of reusable React components,enhancing application flexibility and
+    maintainability, leading to a 25% increase in developer productivity and a 30% reduction in bug fixes.
+    Managed complex application state using Redux and Context API, ensuring seamless state synchronization. Diagnosed and resolved complex bugs and performance issues by systematically analyzing code and using console
+    logs to trace errors.
+    `,
+    technologies: ["Javascript", "React.js", "Next.js", "Tailwind CSS"],
+  },
+  {
+    year: "May 2019 - June 2019",
+    role: "Summer Trainee",
+    company: "RCPL",
+    description: ` Worked on Android "Travel Expense Manager" Application Development.Created with the tech stack listed below:
+    IDE : Android Studio 
+    Programming Language : Java
+    UI : XML Layout files & Android UI Toolkit
+    Database : SQLite `,
+    technologies: ["Java", "XML", "Android Studio"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Cinematic Database",
+    image: "/assets/projects/CD.png", // Path relative to /public
+    description:
+      "A Interactive platform designed to provide users with a comprehensive and engaging movie discovery experience. Built using ReactJS, this application showcases a modern and visually appealing user interface that simplifies the process of finding and exploring movies.",
+    technologies: ["HTML", "TailwindCSS", "React", "Redux", "JavaScript"],
+  },
+  {
+    title: "HungryHub Food Delivery Website",
+    image: "/assets/projects/Hungry.png", // Path relative to /public
+    description:
+      "A modern food delivery platform designed to provide users with a seamless and intuitive experience when browsing restaurant menus, placing orders, and tracking deliveries.",
+    technologies: ["HTML", "CSS", "React", "JavaScript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: "/assets/projects/portfolio.png", // Path relative to /public
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind", "Framer Motion"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bengaluru ",
+  phoneNo: "+91 8310734652",
+  email: "aravindgavappa12@gmail.com",
+};
